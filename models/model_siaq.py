2a9b16c75d1438db0adee568259d70ae91e2cacc
Alif Aditya Wicaksono <81549262+Adddedtiya@users.noreply.github.com>
2025-06-18 00:07:16 +0900

    update info

--- models/model_siaq.py
+++ models/model_siaq.py
@@ -236,11 +236,26 @@ if __name__ == "__main__":
     print("SIAK")
 
     m = LigweightAutoencoderK512(1, 1)
+
+    lldict = torch.load('./weights/q512e32_best_weight_at150.pt', map_location = 'cpu', weights_only = True)
+    m.load_state_dict(lldict['autoencoder'])
     m.eval()
 
-    t = torch.rand(1, 1, 512, 512)
+    xt = torch.rand(1, 1, 512, 512)
 
-    from torchinfo import summary
-    summary(m, input_data = t)
 
-    print(m)
+    x = m.encoder(xt)
+    quantized, inidicies, entropy_aux_loss = m.quantizer(x)
+    print(entropy_aux_loss)
+    print(inidicies.shape)
+    print(inidicies)
+
+    quant_from_indicies = m.quantizer.get_output_from_indices(inidicies)
+    quant_from_indicies = rearrange(quant_from_indicies, 'n h w c -> n c h w')
+    print(quantized.shape, quant_from_indicies.shape)
+
+    print(torch.equal(quant_from_indicies, quantized))
+
+
+
+
